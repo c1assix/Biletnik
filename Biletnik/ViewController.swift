c077ad23df9e30f092bfff2c6953b06cc6a4ea6e
Лев Купчинов <levkupcinov@MacBook-Pro-Lev.local>
2017-11-01 21:01:21 +0300

    Initial Commit

---
+++ Biletnik/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Biletnik
+//
+//  Created by Лев Купчинов on 01.11.17.
+//  Copyright © 2017 Лев Купчинов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
